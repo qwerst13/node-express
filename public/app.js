e026b6680b9f178a40baca436e27f72a69982252
biohazard <qwerst13@gmail.com>
2021-05-27 00:03:55 +0300

    dynamic cart update

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach((item) => {
-    item.textContent = new Intl.NumberFormat('en-US', {
+const toCurrency = (price) => {
+    return new Intl.NumberFormat('en-US', {
         currency: 'usd',
         style: 'currency'
-    }).format(item.textContent);
+    }).format(price);
+}
+
+document.querySelectorAll('.price').forEach((item) => {
+    item.textContent = toCurrency(item.textContent);
 });
 
 const $cart = document.querySelector('#cart');
@@ -14,7 +18,26 @@ if ($cart) {
                 method: 'delete'
             }).then((res) => res.json())
                 .then((cart) => {
-                    console.log(cart);
+                    if (cart.courses.length) {
+                        const html = cart.courses.map((item) => {
+                            return `
+                              <tr>
+                                <td>${item.title}</td>
+                                <td>${item.price}</td>
+                                <td>${item.count}</td>
+                                <td>
+                                  <button class="btn btn-small js-remove material-icons" data-id="${item.id}">clear</button>
+                                </td>
+                              </tr>
+                            `;
+                        }).join('');
+                        $cart.querySelector('tbody').innerHTML = html;
+
+                        $cart.querySelector('.price').textContent = toCurrency(cart.totalPrice);
+
+                    } else {
+                        $cart.innerHTML = '<p>Cart is empty</p>';
+                    }
                 })
         }
     })
